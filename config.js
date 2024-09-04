924cee7cd1c1f72b90c317a41157a11479c88aaf
Fernando <fernandosilvabarreto1@gmail.com>
2024-09-03 21:35:56 -0300

    alteracoes 3

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+        await mongoose.connect('mongodb+srv://qa:cademy@cluster0.dwyxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
         });
         console.log('MongoDB connected...');
     } catch (err) {
@@ -15,5 +12,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-
